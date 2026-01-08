e0cc529f3535176742549226924a88c2084cd9da
Pyprohly <pyprohly@outlook.com>
2026-01-09 01:29:11 +1100

    Don't preload fonts

--- src/fonts.ts
+++ src/fonts.ts
@@ -27,88 +27,110 @@ import {
 export const _antic = Antic({
   subsets: ["latin"],
   weight: ["400"],
+  preload: false,
 })
 export const _architectsDaughter = Architects_Daughter({
   subsets: ["latin"],
   weight: ["400"],
+  preload: false,
 })
 export const _dmSans = DM_Sans({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _firaCode = Fira_Code({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _geist = Geist({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _geistMono = Geist_Mono({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _ibmPlexMono = IBM_Plex_Mono({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _inter = Inter({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _libreBaskerville = Libre_Baskerville({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _merriweather = Merriweather({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _openSans = Open_Sans({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _outfit = Outfit({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _oxanium = Oxanium({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _poppins = Poppins({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _roboto = Roboto({
   subsets: ["latin"],
   weight: ["800"],
+  preload: false,
 })
 export const _robotoMono = Roboto_Mono({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _sourceCodePro = Source_Code_Pro({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _spaceMono = Space_Mono({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
 export const _ubuntuMono = Ubuntu_Mono({
   subsets: ["latin"],
   weight: ["700"],
+  preload: false,
 })
